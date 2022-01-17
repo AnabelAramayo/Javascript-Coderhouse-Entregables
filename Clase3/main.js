b0552f0d6dfb25f412cb09df2e1fb81ea75d0516
AnabelAramayo <anabelaramayo@gmail.com>
2022-01-17 19:51:08 -0300

    Ejer clase 3

---
+++ Clase3/main.js
@@ -0,0 +1,16 @@
+// Clase 3 ciclos e iteraciones
+
+let edadMirtha= "";
+
+
+while (edadMirtha != 94){
+    edadMirtha = prompt('¿Cuántos años tiene Mirtha Legrand?');
+}
+
+
+
+//Visto en clase de Funciones
+function saludar () {
+    console.log("Hola como estas?")
+};
+
